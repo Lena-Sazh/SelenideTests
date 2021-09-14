519c4f2c9cb87733b5e97a7509a3627af3201f6a
Elena <Lena-Sazh@users.noreply.github.com>
2021-09-14 22:36:08 +0300

    Test commit

--- src/test/java/github/SelenideSearch.java
+++ src/test/java/github/SelenideSearch.java
@@ -2,9 +2,12 @@ package github;
 
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selenide.open;
+
 public class SelenideSearch {
     @Test
     void findSelenideRepoPage(){
+        open("https://github.com");
 
     }
 }
